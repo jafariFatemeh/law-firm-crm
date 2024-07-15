167ee42cc8f3ef838deb2023d91b83488f49a1c4
jafware <info@jafware.com>
2024-07-15 17:49:02 +0530

    .

--- frontend/src/services/axiosConfig.js
+++ frontend/src/services/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || 'https://backend-0vwz.onrender.com/api',
 });
 
 export default axiosInstance;
