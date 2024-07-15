5dffc6acbb5009a7613eea02a49cf8092c2aeb0c
jafware <info@jafware.com>
2024-07-15 22:12:02 +0530

    ..

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 // backend/models/User.js
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -20,6 +21,33 @@ const userSchema = new Schema({
     default: Date.now,
   },
 });
+
+userSchema.pre('save', async function(next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
+    // Hash the password with the salt
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    // Replace plaintext password with hashed password
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 // Create a model based on the schema
 const User = mongoose.model('User', userSchema);
